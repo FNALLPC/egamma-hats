58ac7f1a966cd4245511ff920c62b7d1e25b34e4
Nick Smith <nick.smith@cern.ch>
2020-01-03 11:14:29 -0600

    New coffea

--- setup.sh
+++ setup.sh
@@ -11,6 +11,6 @@ source $LCG
 python -m venv --copies $NAME
 source $NAME/bin/activate
 python -m pip install setuptools pip --upgrade
-python -m pip install "coffea>=0.6.25"
+python -m pip install "coffea>=0.6.27"
 sed -i "2a source ${LCG}" $NAME/bin/activate
 ipython kernel install --user --name=$NAME
