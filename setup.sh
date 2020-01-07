3e51eeae3e6b320c04dca6dafa81fd641c9b4cc4
Nick Smith <nick.smith@cern.ch>
2020-01-07 13:02:05 -0600

    Support csh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 NAME=egammaenv
-LCG=/cvmfs/sft.cern.ch/lcg/views/LCG_96python3/x86_64-centos7-gcc8-opt/setup.sh
+LCG=/cvmfs/sft.cern.ch/lcg/views/LCG_96python3/x86_64-centos7-gcc8-opt
 
 if [[ -f $NAME/bin/activate ]]; then
   echo "egammaenv already installed. Run \`source $NAME/bin/activate\` to activate"
   exit 1
 fi
-source $LCG
+source $LCG/setup.sh
 python -m venv --copies $NAME
 source $NAME/bin/activate
 python -m pip install setuptools pip --upgrade
 python -m pip install "coffea>=0.6.27"
-sed -i "2a source ${LCG}" $NAME/bin/activate
-sed -i "4a source ${LCG}" $NAME/bin/activate.csh
+sed -i "2a source ${LCG}/setup.sh" $NAME/bin/activate
+sed -i "4a source ${LCG}/setup.csh" $NAME/bin/activate.csh
 ipython kernel install --user --name=$NAME
