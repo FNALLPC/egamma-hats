b0fab5413a664ee8b99955bd6141323f9b604eee
Nick Smith <nick.smith@cern.ch>
2021-01-03 17:32:25 -0600

    Update to ak1

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 NAME=egammaenv
-LCG=/cvmfs/sft.cern.ch/lcg/views/LCG_96python3/x86_64-centos7-gcc8-opt
+LCG=/cvmfs/sft.cern.ch/lcg/views/LCG_98python3/x86_64-centos7-gcc9-opt
 
 if [[ -f $NAME/bin/activate ]]; then
   echo "egammaenv already installed. Run \`source $NAME/bin/activate\` to activate"
@@ -10,8 +10,13 @@ fi
 source $LCG/setup.sh
 python -m venv --copies $NAME
 source $NAME/bin/activate
-python -m pip install setuptools pip --upgrade
-python -m pip install "coffea>=0.6.27"
+LOCALPATH=$(python -c 'import sys; print(f"{sys.prefix}/lib/python{sys.version_info.major}.{sys.version_info.minor}/site-packages")')
+export PYTHONPATH=${LOCALPATH}:$PYTHONPATH
+python -m pip install setuptools pip wheel --upgrade
+python -m pip install --upgrade matplotlib
+python -m pip install "coffea==0.7.0rc1"
 sed -i "2a source ${LCG}/setup.sh" $NAME/bin/activate
+sed -i "3a export PYTHONPATH=${LOCALPATH}:\$PYTHONPATH" $NAME/bin/activate
 sed -i "4a source ${LCG}/setup.csh" $NAME/bin/activate.csh
+sed -i "5a setenv PYTHONPATH ${LOCALPATH}:\$PYTHONPATH" $NAME/bin/activate.csh
 ipython kernel install --user --name=$NAME
