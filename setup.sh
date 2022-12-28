6acd1b967f8c652fb209dc82a6f979d1a059b6b3
GraceCummings <cummingsge@vcu.edu>
2022-12-27 19:45:40 -0600

    updated to most recetn LCG. Runs with xrootd warning, but does not seem to impact performance. coffea in lcg_rel102

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 NAME=egammaenv
-#LCG=/cvmfs/sft.cern.ch/lcg/views/LCG_98python3/x86_64-centos7-gcc9-opt
-LCG=/cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos7-gcc11-opt #new env
+LCG=/cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos7-gcc11-opt #as of Dec 2022, most recent LCG
 
 if [[ -f $NAME/bin/activate ]]; then
   echo "egammaenv already installed. Run \`source $NAME/bin/activate\` to activate"
@@ -13,11 +12,4 @@ python -m venv --copies $NAME
 source $NAME/bin/activate
 LOCALPATH=$(python -c 'import sys; print(f"{sys.prefix}/lib/python{sys.version_info.major}.{sys.version_info.minor}/site-packages")')
 export PYTHONPATH=${LOCALPATH}:$PYTHONPATH
-#python -m pip install setuptools pip wheel --upgrade
-#python -m pip install --upgrade matplotlib
-#python -m pip install "coffea==0.7.0rc1" hist
-#sed -i "2a source ${LCG}/setup.sh" $NAME/bin/activate
-#sed -i "3a export PYTHONPATH=${LOCALPATH}:\$PYTHONPATH" $NAME/bin/activate
-#sed -i "4a source ${LCG}/setup.csh" $NAME/bin/activate.csh
-#sed -i "5a setenv PYTHONPATH ${LOCALPATH}:\$PYTHONPATH" $NAME/bin/activate.csh
 ipython kernel install --user --name=$NAME
